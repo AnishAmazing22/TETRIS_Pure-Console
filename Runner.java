f0c031fbd44b18d4bbd96b237ba1695a57218384
Anish Kommireddy <562601@my.cuhsd.org>
2024-12-17 21:10:16 -0800

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -1,1 +1,17 @@
 
+/**
+ * the tester program to run the code
+*/
+public class Runner
+{
+    /**
+     * runs the code and runs the tetris game inside of it
+    */
+    public static void main(String[] args) 
+    {
+        //runs the tetris game
+        Tetris.run();
+        
+        
+    }
+}
