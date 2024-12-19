efc36017927391bda7686a628a2fe3ef5fdbf355
Anish Kommireddy <562601@my.cuhsd.org>
2024-12-18 17:47:16 -0800

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -10,6 +10,9 @@ public class Runner
     */
     public static void main(String[] args) 
     {
+        System.out.println("Welcome to Tetris!\n\nControls:\n1. ENTER: progress a tick\n2. A: move left\n3. D: move right\n4. S or hold ENTER: move down\n5. SPACE: drop block\n");
+        System.out.println("Spam ENTER at a consistent, high speed to play properly\n");
+        
         Scanner scanner = new Scanner(System.in);
         System.out.println("Do you want to play Tetris in hard or easy");
         if(!(scanner.nextLine()).equals("hard")){
