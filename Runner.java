7fea2692ac83fdbb5cc2596fe322f14c24ccd606
Anish Kommireddy <562601@my.cuhsd.org>
2024-12-18 17:36:18 -0800

    Update Runner.java
    added easy mode

--- Runner.java
+++ Runner.java
@@ -1,14 +1,20 @@
-
+import java.util.Scanner;
 /**
  * the tester program to run the code
 */
 public class Runner
 {
+    public static boolean easy = false;
     /**
      * runs the code and runs the tetris game inside of it
     */
     public static void main(String[] args) 
     {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to play Tetris in hard or easy");
+        if(!(scanner.nextLine()).equals("hard")){
+            easy=true;
+        }
         //runs the tetris game
         Tetris.run();
         
