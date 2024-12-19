1880a5a22faf07e62fff9915cb72b017f90eb67e
Anish Kommireddy <562601@my.cuhsd.org>
2024-12-18 17:31:15 -0800

    Update Shadow.java

--- Shadow.java
+++ Shadow.java
@@ -1,10 +1,18 @@
+/**
+ * creates a shadow of the block to see where it will fall 
+*/
 public class Shadow {
-    
+    //the position of the shadow
     public int[][] pos = {{0,0},{0,0},{0,0},{0,0}, {1}};
-    
+    /**
+     * Constructor
+    */
     public Shadow(){
         
     }
+    /**
+     * makes the shadow fall to the bottom
+    */
     public void fall(){
         pos[0][1] +=1;
         pos[1][1] +=1;
